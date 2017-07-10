96f0c169caf997aa80f322ea70f261ed3d443492
sdshinghal <sdshinghal@gmail.com>
2017-07-10 10:32:38 +0530

    Update urls.py

--- t1/urls.py
+++ t1/urls.py
@@ -5,8 +5,11 @@ from snippets import views
 urlpatterns = [
     url(r'^snippets/$', views.SnippetList.as_view()),
     url(r'^snippets/(?P<pk>[0-9]+)$', views.SnippetDetail.as_view()),
+    url(r'^$, views.PostList.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
 
 # I've added a comment here
+
+# Let's add some more stuff here
