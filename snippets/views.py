b505ade45d9140c6aff0bbda1e6257b5c148e419
Sakshi <sakshi@Sakshis-MacBook-Air.local>
2017-07-10 09:51:41 +0530

    Made some changes

--- snippets/views.py
+++ snippets/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def first_view(request):
+    return HttpResponse("Here's the first webpage.")
